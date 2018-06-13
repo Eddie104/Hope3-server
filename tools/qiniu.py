2c765c36dbb92d03854d91a820279a29d96e0860
hongjie <32968210@qq.com>
2018-06-13 22:31:24 +0800

    七牛

---
+++ tools/qiniu.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from qiniu import Auth, put_file, etag, urlsafe_base64_encode
+
+def upload_2_qiniu(img_space, img_name, img_local_path):
+	# 需要填写你的 Access Key 和 Secret Key
+	access_key = 'u8204eU35XvUiDcFE-NcctjgVtdUkeEeaR6UObWi'
+	secret_key = 'lFdpeVd89l5u5e7GKKapdqtI-yPAeHxB9NEDJPv-'
+	# 构建鉴权对象
+	q = Auth(access_key, secret_key)
+	# 要上传的空间
+	bucket_name = 'hope3'
+	# 上传到七牛后保存的文件名
+	key = '%s/%s' % (img_space, img_name)
+	# 生成上传 Token，可以指定过期时间等
+	token = q.upload_token(bucket_name, key, 3600)
+	# 要上传文件的本地路径
+	localfile = img_local_path
+	ret, info = put_file(token, key, localfile)
+	# print(info)
+	assert ret['key'] == key
+	assert ret['hash'] == etag(localfile)
+
+
+def main():
+    # 上传到七牛
+    for (root, _, files) in os.walk('/usr/share/nginx/html/Hope3/champssports/'):
+        for file in files:
+            upload_2_qiniu('champssports/%s' % file, '0.jpg', os.path.join(root, file))
+
+if __name__ == '__main__':
+    main()
+    print('done')
