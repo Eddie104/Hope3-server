e973e3806410def410ead2860b4786edb396a02a
hongjie <32968210@qq.com>
2018-06-16 21:29:30 +0800

    自动根据number关联款型

--- app/controller/admin/pendingGoods.js
+++ app/controller/admin/pendingGoods.js
@@ -62,6 +62,52 @@ class PendingGoodsController extends Controller {
         await this.ctx.model.PendingGoods.update({ _id }, { $set: { is_checked: true } });
         this.success();
     }
+
+    async autoConnectByNumber() {
+        const pendingGoodsArr = await this.ctx.model.PendingGoods.find({ is_checked: { $ne: true } }, {
+            name: 1,
+            number: 1,
+            url: 1,
+            size_price_arr: 1,
+            imgs: 1,
+            platform: 1,
+            platform_id: 1,
+        });
+        let pendingGoods = null;
+        let goodsColor = null;
+        let goodsType = null;
+        for (let i = 0; i < pendingGoodsArr.length; i++) {
+            pendingGoods = pendingGoodsArr[i];
+            goodsColor = await this.ctx.model.GoodsColor.findOne({ number: pendingGoods.number }, { goods_type_id: 1 });
+            if (goodsColor) {
+                goodsType = await this.ctx.model.GoodsType.findOne({ _id: goodsColor.goods_type_id });
+                if (goodsType) {
+                    // 新建商品
+                    const id = await this.ctx.service.createId.getId('Goods');
+                    const goods = new this.ctx.model.Goods({
+                        id,
+                        name: pendingGoods.name,
+                        url: pendingGoods.url,
+                        number: pendingGoods.number,
+                        sku: pendingGoods.size_price_arr,
+                        img: Array.isArray(pendingGoods.imgs) && pendingGoods.imgs.length > 0 ? `${pendingGoods.platform}/${pendingGoods.imgs[0]}` : '',
+                        platform_id: pendingGoods.platform_id,
+                    });
+                    await goods.save();
+
+                    // 配色和款型关联上
+                    await this.ctx.model.GoodsColor.update({
+                        _id: goodsColor._id,
+                    }, { $addToSet: { goods_id_arr: goods._id } });
+
+                    // 把商品和配色关联上
+                    await this.ctx.model.Goods.update({ _id: goods._id }, { $set: { goods_color_id: goodsColor._id } });
+                    await this.ctx.model.PendingGoods.update({ _id: pendingGoods._id }, { $set: { is_checked: true } });
+                }
+            }
+        }
+        this.success();
+    }
 }
 
 module.exports = PendingGoodsController;
--- app/router.js
+++ app/router.js
@@ -56,6 +56,8 @@ module.exports = app => {
 
     router.get('/admin/fetch_brand_and_category', app.oAuth2Server.authenticate(), controller.admin.pendingGoods.fetchBrandAndCategory);
 
+    router.get('/admin/auto_connect_by_number', app.oAuth2Server.authenticate(), controller.admin.pendingGoods.autoConnectByNumber);
+
     // 款型相关
     router.post('/admin/add_goods_type', app.oAuth2Server.authenticate(), controller.admin.goodsType.add);
     // 关联款型
