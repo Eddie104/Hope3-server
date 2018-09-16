6aa1ae85ff609cbe8e19f0e8f7c41dfb0c3b7dbe
hongjie <32968210@qq.com>
2018-09-16 22:42:42 +0800

    增加pushAdmin.sh

--- app/controller/webhook.js
+++ app/controller/webhook.js
@@ -4,7 +4,7 @@ const Controller = require('../core/baseController');
 
 class WebHookController extends Controller {
     async pushAdmin() {
-        child_process.execFile(path.join(this.config.baseDir, 'app/public/shell/pushAdmin.sh'));
+        child_process.execFile(path.join(this.config.baseDir, './shell/pushAdmin.sh'));
         this.success();
     }
 }
