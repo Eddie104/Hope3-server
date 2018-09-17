772bbe1841a1d804ad18625dc328eeb0f826b1b9
hongjie <32968210@qq.com>
2018-09-17 15:23:04 +0800

    fixed a bug

--- shell/pushAdmin.sh
+++ shell/pushAdmin.sh
@@ -1,1 +1,1 @@
-cd /home/Hope3-admin && git pull && \cp /home/Hope3-admin/dist/*.js /usr/local/openresty/nginx/html/ && \cp /home/Hope3-admin/dist/*.css /usr/local/openresty/nginx/html/ && \cp /home/Hope3-admin/dist/index.html /usr/local/openresty/nginx/html/index.html
+cd /home/Hope3-admin && git pull && \cp /home/Hope3-admin/dist/*.js /usr/local/openresty/nginx/html/ && \cp /home/Hope3-admin/dist/*.css /usr/local/openresty/nginx/html/ && \cp /home/Hope3-admin/dist/index.html /usr/local/openresty/nginx/html/hope3.html
