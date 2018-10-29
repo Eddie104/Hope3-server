f64f8892c3629a63fff76b5bc20f7c751dba0471
hongjie <32968210@qq.com>
2018-10-30 07:38:05 +0800

    spider

--- app/schedule/spider.js
+++ app/schedule/spider.js
@@ -15,7 +15,7 @@ class Spider extends Subscription {
             // │    │    └─────────────── hour(0 - 23)
             // │    └──────────────────── minute(0 - 59)
             // └───────────────────────── second(0 - 59, optional)
-            cron: '0 15 23 * * *',
+            cron: '0 45 7 * * *',
             cronOptions: { tz: 'Asia/Shanghai' },
             type: 'worker',
             immediate: false,
