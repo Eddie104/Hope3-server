d8372fbc9628977685bdd7d8148fe43ae9e44bb9
hongjie <32968210@qq.com>
2019-01-06 22:42:46 +0800

    spider

--- app/service/spider.js
+++ app/service/spider.js
@@ -7,7 +7,7 @@ class SpiderService extends Service {
     async run() {
         // const ls = child_process.spawn('cd /home/Hope3-spider/ && python spider.py goat');
         // const ls = child_process.spawn('python', [ '/home/Hope3-spider/spider.py', 'stockx' ]);
-        const ls = child_process.spawn('python', [ '/home/Hope3-spider/spider.py', 'nike', 'common' ]);
+        const ls = child_process.spawn('python', [ '/home/Hope3-spider/spider.py', 'stadiumgoods', 'common' ]);
         ls.stdout.on('data', data => {
             this.ctx.logger.info('stdout: ' + data);
         });
